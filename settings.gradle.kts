c0c25bf5b7dbf18cf12ad9c2884febbea85126bc
Benn Roth <theatomicoption@users.noreply.github.com>
2025-04-28 20:06:57 -0700

    Partial update to 1.21.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
         maven("https://maven.dediamondpro.dev/releases")
         maven("https://repo.polyfrost.org/releases")
     }
-    val egtVersion = "0.6.6"
+    val egtVersion = "0.6.7"
     plugins {
         id("gg.essential.multi-version.root") version egtVersion
     }
@@ -39,9 +39,10 @@ listOf(
     //"1.21-forge",
     //"1.21-neoforge",
     //"1.21-fabric",
-    "1.21.4-forge",
-    "1.21.4-neoforge",
-    "1.21.4-fabric",
+    //"1.21.4-forge",
+    //"1.21.4-neoforge",
+    //"1.21.4-fabric",
+    "1.21.5-fabric",
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
