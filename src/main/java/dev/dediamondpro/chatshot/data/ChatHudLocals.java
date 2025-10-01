d515ebead870560eefe6a744d8acbb8e67a1c270
Gravy Boat <gravy@thatgravyboat.tech>
2025-10-01 16:53:35 -0230

    fix: copy not using full message

--- src/main/java/dev/dediamondpro/chatshot/data/ChatHudLocals.java
+++ src/main/java/dev/dediamondpro/chatshot/data/ChatHudLocals.java
@@ -1,7 +1,11 @@
 package dev.dediamondpro.chatshot.data;
 
+import net.minecraft.network.chat.Component;
+
 public interface ChatHudLocals {
     int chatShot$getChatY();
 
     int chatShot$getChatBackgroundColor();
+
+    Component chatshot$getMessageForLine(int index);
 }
